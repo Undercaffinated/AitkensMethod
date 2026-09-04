6a86a78f7944c09a76a85217c684a340f15136bb
Undercaffinated <90402996+Undercaffinated@users.noreply.github.com>
2026-09-03 20:38:42 -0500

    Delete aitkens_method/target directory

--- aitkens_method/target/debug/deps/aitkens_method-3f45e5903eb89d38.d
+++
@@ -1,5 +0,0 @@
-/home/n/Github/AitkensMethod/aitkens_method/target/debug/deps/aitkens_method-3f45e5903eb89d38.d: src/main.rs
-
-/home/n/Github/AitkensMethod/aitkens_method/target/debug/deps/libaitkens_method-3f45e5903eb89d38.rmeta: src/main.rs
-
-src/main.rs:
--- aitkens_method/target/debug/deps/aitkens_method-fce4f0bcf6bc33c3.d
+++
@@ -1,5 +0,0 @@
-/home/n/Github/AitkensMethod/aitkens_method/target/debug/deps/aitkens_method-fce4f0bcf6bc33c3.d: src/main.rs
-
-/home/n/Github/AitkensMethod/aitkens_method/target/debug/deps/libaitkens_method-fce4f0bcf6bc33c3.rmeta: src/main.rs
-
-src/main.rs:
